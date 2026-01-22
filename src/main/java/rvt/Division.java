762ea0c3f44fc97ce655b3efc81b03f5c4ec8ad5
24DP3GMila <A240237GM@rvt.lv>
2026-01-22 14:08:17 +0200

    DivisionPractise.java nav pabeigts

---
+++ src/main/java/rvt/Division.java
@@ -0,0 +1,26 @@
+package rvt;
+
+import java.util.*;
+
+public class Division{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numerator = 0, divisor = 0;
+        boolean goodData = false;
+        while (!goodData) {
+            try {
+                System.out.print("Enter the numerator: ");
+                numerator = scanner.nextInt();
+                System.out.print("Enter the divisor: ");
+                divisor = scanner.nextInt();
+                System.out.println(numerator + " / " + divisor + "is " + (numerator/divisor));
+            } catch (ArithmeticException ex) {
+                System.out.println("You can't divide " + numerator + "by " + divisor);
+            } catch (InputMismatchException ex) {
+                System.out.println("You entered bad data.");
+                System.out.println("Please try again.");
+            }
+        }
+
+    }
+}
