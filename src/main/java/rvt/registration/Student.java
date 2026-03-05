7f40335834f6d6c6402dad34ff7dc99a94d64b79
24DP3GMila <A240237GM@rvt.lv>
2026-03-05 12:43:18 +0200

    Student registration

---
+++ src/main/java/rvt/registration/Student.java
@@ -0,0 +1,12 @@
+package rvt.registration;
+
+public class Student {
+    private String name;
+    private String surname;
+    private String eMail;
+    private String personalIdentificationNumber;
+    private String regitrationData;
+    private String registrationTime;
+
+    
+}
