0750a80c8eef17cc22ebbd612e676057cb33860a
Georgijs Milancejs <georgijs.milancejs@gmail.com>
2026-05-17 19:18:15 +0300

    Person.java

--- src/main/java/rvt/Person.java
+++ src/main/java/rvt/Person.java
@@ -3,18 +3,18 @@ package rvt;
 public class Person {
     private String name;
     private String address;
-    public Person(String name, String adress) {
+    public Person(String name, String address) {
         this.name = name;
-        this.address = adress;
+        this.address = address;
     }
     public String getName() {
         return this.name;
     }
-    public String getAdress() {
+    public String getAddress() {
         return this.address;
     }
     @Override
     public String toString() {
-        return this.name + "\n" + this.address;
+        return this.name + "\n  " + this.address;
     }
 }
